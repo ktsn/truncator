9bfd280642197ca60c9d67878c1d4b3f6f4d1565
katashin <ktsn55@gmail.com>
2017-03-03 22:47:39 +0900

    Tweak webpack.config

--- scripts/webpack.config.test.js
+++ scripts/webpack.config.test.js
@@ -15,8 +15,7 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.js$/, loader: 'babel', exclude: /node_modules/ },
-      { test: /\.json$/, loader: 'json' }
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ }
     ]
   }
 }
